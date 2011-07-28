534d2c30ef55bcfb15d4c5e3e913ca29100a61d8
Brandon Sandrowicz <bsandrow@gmail.com>
2011-07-27 22:43:49 -0700

    A little bit of cleanup in flvid

--- flvid/__init__.py
+++ flvid/__init__.py
@@ -2,19 +2,23 @@ import re
 
 registered_scrapers = []
 
-class Controller(object):
-    def determine_scraper(self, url):
-        for cls in registered_scrapers:
-            scraper = cls()
-            if scraper.should_scrape_url(url):
-                return cls
-        return None
+def determine_scraper(url):
+    ''' Match the url against the list of registered scrapers and return
+    the class of the first matching scraper '''
+    for cls in registered_scrapers:
+        scraper = cls()
+        if scraper.should_scrape_url(url):
+            return cls
+    return None
 
-    def get_video(self, url, scraper=None):
-        if scraper is None:
-            sc      = self.determine_scraper(url)
-            scraper = sc()
-        return scraper.scrape_video(url)
+def get_video(url, scraper=None):
+    ''' Return a Video object containing all of the scraped metadata for
+    a video. The scraper can be specified, otherwise determine_scraper() will
+    be used to determine the scraper to use. '''
+    if scraper is None:
+        scraper_class   = determine_scraper(url)
+        scraper         = scraper_class()
+    return scraper.scrape_video(url)
 
 class Video(object):
     format_fallback_order = None
@@ -32,14 +36,27 @@ class Video(object):
         return None
 
     def __str__(self):
-        return "<%s (format_fallback_order: %s, all_formats: %s, format_urls: %s)>" \
-                    % (self.__class__.__name__, self.format_fallback_order, self.all_formats, self.format_urls)
+        ffo = 'format_fallback_order: %s' % self.format_fallback_order
+        cls = self.__class__.__name__
+        afs = 'all_formats: %s' % self.all_formats
+        fus = 'format_urls: %s' % self.format_urls
+        return "<%s (%s, %s, %s)>" % (cls, ffo, afs, fus)
 
 class Scraper(object):
+    ''' A base object for video page scrapers. At least initially, this base
+    class is mostly here to help form some sort of structure, and to add the
+    minimal amount of drop-in functionality that is
+    Scraper.should_scrape_url() '''
+
+    # Sub-classes should set this to whatever video class they use.
     video_class = None
 
     def should_scrape_url(self, url):
-        """ Determine if this processor thinks it should process this url """
+        ''' Return True/False based on whether or not this scraper is able
+        process the url. This is a generic version that should work in most
+        cases, but there are definitely cases where you will want to overload
+        this (e.g. breaking apart the query string into params to make sure
+        that certain params exist).'''
         if type(self.url_pattern) == str:
             return self.url.starts_with(self.url_pattern)
         else:
@@ -47,7 +64,9 @@ class Scraper(object):
             return m is not None
 
     def scrape_video(self, url):
-        """ (Stub) Extract a direct flash video url from the passed url. """
+        ''' A stub for the main functionality of a Scraper class. This is meant
+        to return an object of class self.video_class (which should be a
+        sub-class of Video).'''
         raise UnimplementedError('Function unimplemented')
 
 class UnimplementedError(Exception):
